2c7ecdf2ae468d7ac972a1325cde101bc06afb28
aven <wangbiwen_88@qq.com>
2016-01-03 20:23:28 +0800

    create the models for books.

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from models import Publisher
+from models import Author
+from models import Book
+
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
