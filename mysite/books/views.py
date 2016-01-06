d1ba9aea4d0bc0f19e011e97506d9d9cd1069270
aven <wangbiwen_88@qq.com>
2016-01-06 14:08:47 +0800

    modify two files: views.py and search_form.html create a new file: search_results.html

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from models import Book
 
 # Create your views here.
 def search_form(request):
     return render_to_response('search_form.html')
 
+#def search(request):
+#    if 'q' in request.GET:
+#        message = 'You search for %r' % request.GET['q']
+#    else:
+#        message = 'You submitted an empty form.'
+#    return HttpResponse(message)
 def search(request):
-    if 'q' in request.GET:
-        message = 'You search for %r' % request.GET['q']
+    if 'q'in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render_to_response('search_results.html', {'books': books, 'query': q})
     else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        return render_to_response('search_form.html', {'error': True})
